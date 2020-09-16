c77cb5a98045978641f1857afca135f80205944f
kmamamori <kmamamori@miners.utep.edu>
2020-09-15 18:54:43 -0600

    Pipe command completed such as 'ls | sort -r'.

--- main.py
+++ main.py
@@ -16,33 +16,25 @@ import time
 	# 	* If the command fails (with a non-zero exit value N), your shell should print "Program terminated with exit code N."
 #
 # 4. Provides the following built-in commands.
-# 	* The "exit" command should cause your shell to terminate.
-# 	* Change directories with the "cd" command.
+* The "exit" command should cause your shell to terminate.
+* Change directories with the "cd" command.
 #
 # 5. Redirection of input and output (e.g. $ ls > /tmp/files.txt).
 #
-	# 6. Simple pipes (e.g. $ ls | sort -r).
+# 6. Simple pipes (e.g. $ ls | sort -r).
 #
 	# 7. Background tasks (e.g. $ find /etc -print & )
 """
-# - fork()
-# - wait()
-# - chdir()
-# - execve() (you cannot use execvp)
-# - open() and close()
-"""
-- pipe()
-- dup() or dup2()
-"""
 
-
+# receives string
 def redirection(redirection_input):
-    redirection_input_arr = re.split('>|<', redirection_input)
     if '>' in redirection_input:
+        redirection_input_arr = re.split('>|<', redirection_input)
         os.close(1)
         os.open(redirection_input_arr[1], os.O_CREAT | os.O_WRONLY)
-        # sys.stdout = open(redirection_input_arr[1].strip(), "w")
         os.set_inheritable(1, True)
+    else:
+        redirection_input_arr = re.split(' |>|<', redirection_input)
     args = redirection_input_arr[0].split()
     for dir in re.split(":", os.environ['PATH']):
         program = "%s/%s" % (dir, args[0])
@@ -50,7 +42,25 @@ def redirection(redirection_input):
             os.execve(program, args, os.environ)
         except FileNotFoundError:
             pass
-    os.write(2, ("Child:    Error: Could not exec %s.\n Command may not found.\n\n" % args[0]).encode())
+    os.write(
+        2, ("Child:    Error: Could not exec %s.\n Command may not found.\n\n" % args[0]).encode())
+
+# receives array
+def changingDir(kbd_input_arr):
+    try:
+        os.chdir(kbd_input_arr[1])
+    except:
+        os.write(
+            2, ("Something went wrong. Check the path you entered.\n").encode())
+        pass
+
+# recieve string
+def check_command(cmd):
+    if "cd" in cmd:
+        changingDir(cmd.split(' '))
+    else:
+        redirection(cmd)
+
 
 while True:
     if 'PS1' in os.environ:
@@ -63,16 +73,31 @@ while True:
     if 'exit' in kbd_input_arr:
         os.write(1, ("EXIT from Shell\n").encode())
         sys.exit(1)
-    elif 'cd' in kbd_input_arr:
-        print("\n"+kbd_input_arr[1]+"\n")
-        try:
-            os.chdir(kbd_input_arr[1])
-            # os.write(1, (os.getcwd()).encode())
-        except:
-            os.write(
-                2, ("Something went wrong. Check the path you entered.\n").encode())
-            pass
-
+    elif 'cd' in kbd_input_arr and not '|' in kbd_input_str:
+        check_command(kbd_input_str)
+    elif '|' in kbd_input_str:
+        pipe_command = kbd_input_str.split('|')
+        pr, pw = os.pipe()
+        for f in (pr, pw):
+            os.set_inheritable(f, True)
+        pipe_rc = os.fork()
+        if pipe_rc < 0:
+            os.write(2, ("Fork failed!").encode())
+            sys.exit(1)
+        elif pipe_rc == 0:
+            os.close(1)
+            os.dup(pw)
+            os.set_inheritable(1, True)
+            for fd in (pr, pw):
+                os.close(fd)
+            check_command(pipe_command[0].strip())
+        else:
+            os.close(0)
+            os.dup(pr)
+            os.set_inheritable(0, True)  # set fd1 inheritable
+            for fd in (pw, pr):
+                os.close(fd)
+            check_command(pipe_command[1].strip())
     else:
         pid = os.getpid()
         rc = os.fork()
@@ -86,10 +111,4 @@ while True:
                 redirection(kbd_input_str)
             sys.exit(1)
         else:  # parent
-            # os.write(1, ("Parent!").encode())
             childPidCode = os.wait()
-            # os.write(1, ("The child will terminate with this").encode())
-
-    # print(kbd_input)  ## TEST
-    # rc = os.fork()
-    # print(os.getpid()) ## TEST
