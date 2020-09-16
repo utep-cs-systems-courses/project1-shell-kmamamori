a7d79bc3c4dc19be7f89efa78a9029e578710aa4
kmamamori <kmamamori@miners.utep.edu>
2020-09-15 21:59:23 -0600

    Clean the file and add comments.

--- shell/main.py
+++ shell/main.py
@@ -5,29 +5,6 @@ import sys
 import re
 import time
 
-"""
-# 1. Prints a prompt string specified by shell variable PS1 when expecting a command (if PS1 is not set, the default prompt should be "$ ").
-#
-# 2. Accepts the standard unix command shell syntex for specifying commands and parameters
-# 	* Your shell should create a child process that uses execve to run the command with its parameters.
-# 	* If an absolute path is not specified, your shell should instead find it using the $PATH environment variable.
-# 	* The parent process should wait for the child to terminate before printing another command prompt.
-#
-	# 3. Handles expected user error.
-	# 	* Prints "command not found" if the command is not found. For example, if the user tries to run program `lx` and this program doesn't exist, the shell should respond with "lx: command not found".
-	# 	* If the command fails (with a non-zero exit value N), your shell should print "Program terminated with exit code N."
-#
-# 4. Provides the following built-in commands.
-* The "exit" command should cause your shell to terminate.
-* Change directories with the "cd" command.
-#
-# 5. Redirection of input and output (e.g. $ ls > /tmp/files.txt).
-#
-# 6. Simple pipes (e.g. $ ls | sort -r).
-#
-	# 7. Background tasks (e.g. $ find /etc -print & )
-"""
-
 # receives string
 def redirection(redirection_input):
     if '>' in redirection_input:
@@ -65,12 +42,12 @@ def check_command(cmd):
 
 
 while True:
-    if 'PS1' in os.environ:
+    if 'PS1' in os.environ: #if exist
         os.write(1, (os.environ['PS1']).encode())
     else:
         os.write(1, os.getcwd().encode())  # print prompt
     kbd_input_str = input(' $')  # get keyboard input
-    kbd_input_arr = kbd_input_str.split(' ')
+    kbd_input_arr = kbd_input_str.split(' ') #make str an array
 
     if 'exit' in kbd_input_arr:
         os.write(1, ("EXIT from Shell\n").encode())
@@ -86,19 +63,19 @@ while True:
         if pipe_rc < 0:
             os.write(2, ("Fork failed!").encode())
             sys.exit(1)
-        elif pipe_rc == 0:
-            os.close(1)
+        elif pipe_rc == 0: #child
+            os.close(1) # close the output
             os.dup(pw)
             os.set_inheritable(1, True)
             for fd in (pr, pw):
-                os.close(fd)
+                os.close(fd) #uneeded port
             check_command(pipe_command[0].strip())
-        else:
-            os.close(0)
+        else: #parent
+            os.close(0) #close input
             os.dup(pr)
             os.set_inheritable(0, True)  # set fd1 inheritable
             for fd in (pw, pr):
-                os.close(fd)
+                os.close(fd) #uneeded port
             check_command(pipe_command[1].strip())
     else:
         pid = os.getpid()
